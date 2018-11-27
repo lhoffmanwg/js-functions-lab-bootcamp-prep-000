ef5c0dd137053b249bb0886fc2f3e3075c061d88
Laura Hoffman <lhoffmanwg@gmail.com>
2018-11-27 05:48:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 // write your code below!
-name = "Laura"
-holiday = "Mother's Day"
-days = "20"
 
-function happyHolidays(){
-  return "Happy holidays!"
-}
-function happyHolidaysTo(name){
-  return `Happy holidays, ${name}!`
-}
-function happyHolidayTo(holiday,name){
-  return `Happy ${holiday}, ${name}!`
-}
-function holidayCountdown(holiday, days){
-  return `It's ${days} days until ${holiday}!`
+function happyHolidays() {
+  return "Happy holidays!";
 }
 
-happyHolidaysTo(name)
-happyHolidayTo(holiday, name)
-holidayCountdown(holiday, days)
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`;
+}
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`;
+}
+
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`;
+}
